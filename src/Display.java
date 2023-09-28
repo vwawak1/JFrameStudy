293c80d411b8a865776d2f725d1d1b34825e7318
Vincent <v.wawak1@gmail.com>
2023-09-28 12:22:13 -0400

    feat: Added BGC and comments

--- src/Display.java
+++ src/Display.java
@@ -1,19 +1,22 @@
 //JMJ
+import java.awt.Color;
 import javax.swing.JFrame;
 
 public class Display extends JFrame {
 	
 	//Constructor for the display
 	public Display(int width, int height) {
-		//set window title, on close event, and resizing
-		setSize(width, height);
-		setTitle("Snek Game");
-		setDefaultCloseOperation(EXIT_ON_CLOSE);
-		setResizable(false);
 		
-		//Centers the window and makes visible
-		setLocationRelativeTo(null);
-		setVisible(true);
+		setSize(width, height); //Set size of window
+		setTitle("Snek Game"); //Set title of window
+		setDefaultCloseOperation(EXIT_ON_CLOSE); //Set close operation
+		setResizable(false); //Set whether the window should be resized
+		setLocationRelativeTo(null); //Center the window
+		setVisible(true); //Set window to appear visible
+		//ImageIcon logo = new ImageIcon("/filename/");
+		//setIconImage(logo.getImage());
+		getContentPane().setBackground(new Color(222, 207, 131));
+		
 	}
 	
 
